c9852f4a49bbc4915d38e0d17deddca905fba80f
zholudev111 <zholudev111@gmail.com>
2023-10-23 23:28:20 +0300

    added 08 task

---
+++ 08_fetch_json_filtering/index.js
@@ -0,0 +1,58 @@
+const fs = require('fs');
+const path = require('path');
+process.env.NODE_TLS_REJECT_UNAUTHORIZED='0'
+const arrEndpoints = [
+    "https://jsonbase.com/sls-team/json-793",
+    "https://jsonbase.com/sls-team/json-955",
+    "https://jsonbase.com/sls-team/json-231",
+    "https://jsonbase.com/sls-team/json-931",
+    "https://jsonbase.com/sls-team/json-93",
+    "https://jsonbase.com/sls-team/json-342",
+    "https://jsonbase.com/sls-team/json-770",
+    "https://jsonbase.com/sls-team/json-491",
+    "https://jsonbase.com/sls-team/json-281",
+    "https://jsonbase.com/sls-team/json-718",
+    "https://jsonbase.com/sls-team/json-310",
+    "https://jsonbase.com/sls-team/json-806",
+    "https://jsonbase.com/sls-team/json-469",
+    "https://jsonbase.com/sls-team/json-258",
+    "https://jsonbase.com/sls-team/json-516",
+    "https://jsonbase.com/sls-team/json-79",
+    "https://jsonbase.com/sls-team/json-706",
+    "https://jsonbase.com/sls-team/json-521",
+    "https://jsonbase.com/sls-team/json-350",
+    "https://jsonbase.com/sls-team/json-64"
+]
+const fetch = require('node-fetch');
+
+const getJson = async (url) => {
+    const response = await fetch(url);
+    const json = await response.json();
+    return json;
+}
+
+const binaryTreeSearch = (object, key, endpoint) => {
+    const arr = Object.keys(object);
+    const res = arr.find((item) => item === key && object[item] === true);
+    if (!res) {
+        return console.log(`[Error] ${endpoint} isDone false`);
+    }
+    if (res) {
+        return console.log(`[Success] ${endpoint} isDone true`);
+    }else{
+        for (let i = 0; i < arr.length; i++) {
+            if (typeof object[arr[i]] === "object") {
+                binaryTreeSearch(object[arr[i]], key);
+            }
+        }
+    }
+}
+
+const main = async () => {
+    for await (const endpoint of arrEndpoints) {
+        const json = await getJson(endpoint);
+        binaryTreeSearch(json, "isDone" , endpoint)
+    }
+}
+main();
+
